3c6933d0fe4110b811b07734ede60c3f26d5505b
Ashdof <mzigah4@gmail.com>
2022-12-15 10:57:08 +0000

    Task 2 - implementation of the opcode that removes the element at the top of the stack

--- monty.h
+++ monty.h
@@ -38,6 +38,7 @@ typedef struct instruction_s
 } instruction_t;
 
 
+void pop(stack_t **stack, unsigned int line_cnt);
 void push(stack_t **stack, unsigned int line_cnt);
 void pint(stack_t **stack, unsigned int line_cnt);
 void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)));
